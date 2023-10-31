2521362d0bf6e227f31e692c24d66a1d8723aa55
jitendra4545 <jitendra45.js45@gmail.com>
2023-10-31 06:39:32 +0530

    db successfully connected and the server also running weel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
+require("dotenv").config()
 const express = require('express')
+const { connection } = require("./config/db")
 const app=express()
 
 app.get("/",(req,res)=>{
@@ -7,5 +8,13 @@ app.get("/",(req,res)=>{
 })
 
 
-
+app.listen(process.env.port,async(req,res)=>{
+    try{
+      await connection
+      console.log("Server Successfully connected to DB")
+    }catch(err){
+      console.log("Unable to connect with DB")
+    }   
+     console.log(`Server running on Port ${process.env.port}`)
+})
 
